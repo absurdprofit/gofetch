39fb92041ec2796e84fe60704378d14c4dca8e67
Nathan Johnson <50880261+nxtexe@users.noreply.github.com>
2023-07-30 09:39:14 -0500

    Change middleware return type to None

--- src/common/types.ts
+++ src/common/types.ts
@@ -57,10 +57,11 @@ export interface ResponseConfig<B> extends GofetchResponseInit {
 
 export type ResponseConfigReturn<B = any> = Pick<ResponseConfig<B>, 'body' | 'headers'>;
 
+export type None = null | undefined | void;
 export interface Middleware<B = any> {
-  onRequest?(config: Gofetch<Request>): Promise<RequestConfig<B> | void> | RequestConfig<B> | void;
-  onResponse?(config: Gofetch<Response>): Promise<ResponseConfigReturn<B> | void> | ResponseConfigReturn<B> | void;
-  onError?(error: GofetchError, controller: RetryController): Promise<ResponseConfigReturn<B> | void> | ResponseConfigReturn<B> | void;
+  onRequest?(config: Gofetch<Request>): Promise<RequestConfig<B> | None> | RequestConfig<B> | None;
+  onResponse?(config: Gofetch<Response>): Promise<ResponseConfigReturn<B> | None> | ResponseConfigReturn<B> | None;
+  onError?(error: GofetchError, controller: RetryController): Promise<ResponseConfigReturn<B> | None> | ResponseConfigReturn<B> | None;
 }
 
 export type GofetchMethod = 'GET' | 'POST' | 'HEAD' | 'OPTIONS' | 'PATCH' | 'DELETE' | 'PUT';
